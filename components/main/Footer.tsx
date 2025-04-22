00abeadfd29fd6ee81046887297287835a01a096
ashutoshpythoncs@gmail.com <ashutoshpythoncs@gmail.com>
2025-04-22 17:29:33 +0530

    DevDisplay Studio

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -13,9 +13,10 @@ const Footer = () => {
   return (
     <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
         <div className="w-full flex flex-col items-center justify-center m-auto">
-            <div className="mb-[20px] text-[15px] text-center">
-            &copy; DevDisplay 2025. All rights reserved
-            </div>
+        <div
+  className="mb-[20px] text-[15px] text-center"
+  dangerouslySetInnerHTML={{ __html: "&copy; DevDisplay 2025. All rights reserved" }}
+></div>
         </div>
     </div>
   )
