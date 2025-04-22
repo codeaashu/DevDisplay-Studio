dbba23c5bdfb2c9e8401072304023e0c47f1d4a3
ashutoshpythoncs@gmail.com <ashutoshpythoncs@gmail.com>
2025-04-22 17:32:59 +0530

    DevDisplay Studio

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -12,12 +12,11 @@ import { FaYoutube } from "react-icons/fa";
 const Footer = () => {
   return (
     <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
-        <div className="w-full flex flex-col items-center justify-center m-auto">
-        <div
-  className="mb-[20px] text-[15px] text-center"
-  dangerouslySetInnerHTML={{ __html: "&copy; DevDisplay 2025. All rights reserved" }}
-></div>
+      <div className="w-full flex flex-col items-center justify-center m-auto">
+        <div className="mb-[20px] text-[15px] text-center">
+          © DevDisplay 2025. All rights reserved
         </div>
+      </div>
     </div>
   )
 }
